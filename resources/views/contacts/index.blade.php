cacf843e16093a825895a7c0a748bb0246c802ae
João Paulo Leite Costa <joaopaulotds@gmail.com>
2025-03-24 14:46:47 -0300

    Table to list contacts

--- resources/views/contacts/index.blade.php
+++ resources/views/contacts/index.blade.php
@@ -1,15 +1,30 @@
 @extends('layouts.welcome')
 @section('content')
     <a href="{{ route('contacts.create') }}">Novo Contato</a>
-    <ul>
-        @foreach($contacts as $contact)
-            <li>{{ $contact->name }} - {{ $contact->email }} - {{ $contact->phone }}
-                <a href="{{ route('contacts.edit', $contact) }}">Editar</a>
-                <form action="{{ route('contacts.destroy', $contact) }}" method="POST">
-                    @csrf @method('DELETE')
-                    <button type="submit">Excluir</button>
-                </form>
-            </li>
-        @endforeach
-    </ul>
+    <table border="1">
+        <thead>
+            <tr>
+                <th>Nome</th>
+                <th>Email</th>
+                <th>Telefone</th>
+                <th>Ações</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($contacts as $contact)
+                <tr>
+                    <td>{{ $contact->name }}</td>
+                    <td>{{ $contact->email }}</td>
+                    <td>{{ $contact->phone }}</td>
+                    <td>
+                        <a href="{{ route('contacts.edit', $contact) }}">Editar</a>
+                        <form action="{{ route('contacts.destroy', $contact) }}" method="POST" style="display:inline;">
+                            @csrf @method('DELETE')
+                            <button type="submit">Excluir</button>
+                        </form>
+                    </td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
 @endsection
